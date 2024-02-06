f9cc8bb07c5e6cb490339f2232abc7a0155c3cfc
Jaime Alfaro <jaimetadeoalfaro@hotmail.com>
2024-02-06 10:50:04 -0600

    modified:   script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const dropdown__toggle2 = document.querySelector(".dropdown__toggle2");
   const dropdown__menu1 = document.querySelector(".dropdown__menu--1");
   const dropdown__menu2 = document.querySelector(".dropdown__menu--2");
-  // a
+
   dropdown__toggle1.addEventListener("click", function () {
     if (dropdown__menu1.style.maxHeight === "0px") {
       dropdown__menu1.style.maxHeight = "150px";
