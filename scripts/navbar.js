10f872393b1fa89b87e56812ebf850872b30e616
Jaime Alfaro <jaimetadeoalfaro@hotmail.com>
2024-02-18 16:13:11 -0600

    organized scripts

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -38,16 +38,4 @@ document.addEventListener("DOMContentLoaded", function () {
       navbar.style.maxHeight = "1000px";
     }
   });
-
-  const audio = document.querySelector(".crownSound");
-
-  const crown = document.querySelector(".crown");
-
-  crown.addEventListener("mouseenter", crownSound);
-  crown.addEventListener("click", crownSound);
-
-  function crownSound() {
-    audio.volume = 0.1;
-    audio.play();
-  }
 });
