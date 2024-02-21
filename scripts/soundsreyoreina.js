f5808a498e4096bb991dcbcca056efbec60054e8
Jaime Alfaro <jaimetadeoalfaro@hotmail.com>
2024-02-20 18:33:51 -0600

    sounds

--- scripts/soundsreyoreina.js
+++ scripts/soundsreyoreina.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const audio = document.querySelector(".crownSound");
+  audio.volume = 0.1;
 
   const crown = document.querySelector(".crown");
 
-  crown.addEventListener("mouseenter", crownSound);
-  crown.addEventListener("click", crownSound);
-
   function crownSound() {
-    audio.volume = 0.1;
     audio.play();
   }
+
+  crown.addEventListener("mouseenter", crownSound);
+  crown.addEventListener("click", crownSound);
 });
