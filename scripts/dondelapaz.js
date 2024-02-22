adb8336c97ffad2d76d9aea689ac59e4eafd1618
Jaime Alfaro <jaimetadeoalfaro@hotmail.com>
2024-02-22 08:58:14 -0600

    :)

--- scripts/dondelapaz.js
+++ scripts/dondelapaz.js
@@ -1,19 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
-  function randomInt() {
-    return Math.ceil(Math.random() * 6);
+  function randomInt(min, max) {
+    return Math.floor(Math.random() * max) + min;
   }
 
   const cello = document.querySelector(".violonchelo");
 
   const section = document.querySelector(".lapaz__ejemplo__info");
 
-  cello.addEventListener("click", function () {
+  var fadeDelay = 1000;
+  var fadeDuration = 1000;
+
+  cello.addEventListener("click", function (e) {
+    // Aparición de nota musical
+    var div = document.createElement("div");
+    div.className = "image-wrapper";
+    div.style.left = e.pageX + "px";
+    div.style.top = e.pageY + "px";
+
+    var img = document.createElement("img");
+    img.src = `../images/dondelapaz/imgnote${randomInt(1, 3)}.png`;
+    img.alt = "myimage";
+    img.style.width = randomInt(30, 50) + "px";
+
+    div.appendChild(img);
+    document.body.appendChild(div);
+    setTimeout(() => {
+      div.classList.add("hovering");
+    }, 10);
+    setTimeout(function () {
+      div.classList.add("fade-out");
+      setTimeout(function () {
+        div.remove();
+      }, fadeDuration);
+    }, fadeDelay);
+
+    // Reproducción del audio
     var audio = document.createElement("audio");
-    // audio.className = "prueba";
-    var source = document.createElement("source");
-    source.src = `../audio/notes/Note${randomInt()}.mp3`;
+    var audioSrc = document.createElement("source");
+    audioSrc.src = `../audio/notes/Note${randomInt(1, 8)}.mp3`;
     audio.volume = 0.8;
-    audio.append(source);
+    audio.append(audioSrc);
     section.append(audio);
     audio.play();
     setTimeout(() => {
